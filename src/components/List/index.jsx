3e9835e19a575b4d3542de5888ff1487da9124df
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-04-18 17:28:10 +0200

    sidebar styles, hover; almost done

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -5,8 +5,8 @@ import Styles from './List.module.scss';
 function List({ items }) {
     return (
         <ul className={Styles.list}>
-            {items.map((item) => (
-                <li className={item.selected ? Styles.active : null}>
+            {items.map((item, index) => (
+                <li key={index} className={(item.className ? Styles[item.className] : (Styles.active))}>
                     <i>
                         {item.icon ? (item.icon) : (
                             <i className={`${Styles.badge} ${Styles[`badge--${item.color}`]}`}></i>)}
