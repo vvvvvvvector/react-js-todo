cce0398b4586a28edd930a84ac20b272da722309
vvvvvec1or <viktorzhdanovich@gmail.com>
2022-04-19 03:24:17 +0200

    popup done; now you can select badge colors to new folder

--- src/components/Badge/index.jsx
+++ src/components/Badge/index.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import Styles from './Badge.module.scss'
 
-function Badge({ color, clickBadge }) {
+function Badge({ color, clickBadge, activeClass }) {
     return (
-        <i onClick={clickBadge} className={`${Styles.badge} ${Styles[`badge--${color}`]}`}></i>
+        <i onClick={clickBadge} className={`${Styles.badge} ${Styles[`badge--${color}`]} ${activeClass}`}></i>
     );
 }
 
